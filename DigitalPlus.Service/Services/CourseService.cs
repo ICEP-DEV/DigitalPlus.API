2f7da32b36c631b2f1f8973d450644777007a0e2
Bongeka Mdluli <222012937@tut4life.ac.za>
2024-09-19 16:14:25 +0200

    api

---
+++ DigitalPlus.Service/Services/CourseService.cs
@@ -0,0 +1,49 @@
+﻿using DigitalPlus.API.Model;
+using DigitalPlus.Data;
+using DigitalPlus.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalPlus.Service.Services
+{
+    public class CourseService : ICrudInterface<Course>
+    {
+
+        private readonly DigitalPlusDbContext _digitalPlusDbContext;
+
+        public CourseService(DigitalPlusDbContext digitalPlusDbContext)
+        {
+            _digitalPlusDbContext = digitalPlusDbContext;
+        }
+
+        public async Task<Course> Add(Course course)
+        {
+            await _digitalPlusDbContext.Courses.AddAsync(course);
+           await _digitalPlusDbContext.SaveChangesAsync();
+            return course;
+        }
+
+        public Task<Course> Delete(Course t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Course> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Course>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Course> Update(Course t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DigitalPlus.Service/Services/DepartmentService.cs
@@ -0,0 +1,47 @@
+﻿using DigitalPlus.API.Model;
+using DigitalPlus.Data;
+using DigitalPlus.Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalPlus.Service.Services
+{
+    public class DepartmentService : ICrudInterface<Department>
+    {
+        private readonly DigitalPlusDbContext _digitalPlusDbContext;
+
+        public DepartmentService(DigitalPlusDbContext digitalPlusDbContext)
+        {
+            _digitalPlusDbContext = digitalPlusDbContext;
+        }
+        public async Task<Department> Add(Department department)
+        {
+            await _digitalPlusDbContext.Departments.AddAsync(department);
+            await _digitalPlusDbContext.SaveChangesAsync();
+            return department;
+        }
+
+        public Task<Department> Delete(Department t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department> Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Department>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Department> Update(Department t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
