491ae0c1eba5e7e700e02842fe82eb5dd872b8ad
FowardPro <fowardnkuna6@gmail.com>
2024-10-07 10:11:49 +0200

    Changes

--- DigitalPlus.Data/obj/Debug/net8.0/DigitalPlus.Data.AssemblyInfo.cs
+++ DigitalPlus.Data/obj/Debug/net8.0/DigitalPlus.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalPlus.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d0ef672bd0360622e9193d613314d919ec82922")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db326295bb7bc13a8037f9560a604cc3a01d617")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalPlus.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalPlus.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
